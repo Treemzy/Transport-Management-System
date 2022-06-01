025008bc9ba3dc8ee854c0ea58b5bff5c80cab92
Treemzy <Treemzy@gmail.com>
2022-06-01 17:19:55 +0100

    Update settings.py

--- Transport/settings.py
+++ Transport/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
